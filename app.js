5a0379a00ba3cf033128d5bc17d17effbd13fb31
Gabesz007 <szolgalati.torocsik@gmail.com>
2024-08-18 15:38:14 +0200

    basic setup code

---
+++ app.js
@@ -0,0 +1,25 @@
+
+import './styles.css';
+
+console.log('<--> Eddig működik.')
+
+fetch('https://api.tvmaze.com/search/shows?q=girls')
+    .then(result => {
+    if (!result.ok) {
+        throw new Error('Nincsen válasz, HIBA!' + result.statusText);
+    }
+        return result.json(); 
+    })
+    .then(data => {
+        console.log(data); 
+        const movieName = data[0];
+        // if ((movieName.show.premiered.value) = (null)) {
+        //     movieName.show.premiered = ('ismeretlen dátum');
+        // };
+        console.log(`A(z) ${movieName.show.name} című film ${movieName.show.premiered}-án/én lett bemutatva. Műfaját tekintve ${movieName.show.genres}.`);
+    })
+    .catch(error => {
+        console.error('Probléma van a fetchel:', error);
+    });
+
+
